47e3fc87ea3ccc1503650e7b70a6f295ca717dfe
vados60 <vadim.pochernin@gmail.com>
2013-01-04 12:37:11 +0200

    adding entire project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '03c50ebaca2b6c6a35919116069c36a2909fed879e340e25f3032f4d9528615b3459eff6f0ab048842255dfd90e78be4537234a1ffe30fb3ea541e37c46c8ebe'
